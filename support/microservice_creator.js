612a68ff8c8e7a6d5098bb53c338007c53de5739
kendell joseph <kendell@jct.io>
2018-08-04 12:21:24 +0000

    Removes slug, adds slugify to support proccess

--- support/microservice_creator.js
+++ support/microservice_creator.js
@@ -3,22 +3,22 @@
 // * Arc creates microservices using a `worker pool` and a `manifest`
 
 // Arc loads resources it needs for microservice creation
-const { slug } = require(`../dependencies`);
+const { slugify } = require(`../dependencies`);
 
 module.exports = (workerPool, microserviceManifest) => {
   return new Promise((resolve) => {
     // **Given** Arc gets the titles of the microservices from the manifest object
     const titles = Object.keys(microserviceManifest);
-    
+
     // **When** Arc creates microservices by their titles
     const microservices = titles.map((unformattedTitle) => {
       // **And** Arc gets the manifest for the microservice
       const manifest = microserviceManifest[unformattedTitle];
       // **TODO:** remove special characters from the microservice title
-      
+
       // **And** Arc creates a slug from the microservice title
-      const title = slug(String(unformattedTitle).toLocaleLowerCase());
-      
+      const title = slugify(String(unformattedTitle).toLocaleLowerCase());
+
       // **Then** Arc returns the microservice with the `title`, `manifest` and `pool` set
       return { title, manifest, pool: workerPool({title, manifest})};
     });
